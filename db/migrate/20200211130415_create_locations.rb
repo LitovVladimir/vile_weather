d4e18c58e302e6d3da8f63089975ee2536c67e1d
v.litov <v.litov@corp.mail.ru>
2020-02-11 18:23:01 +0300

    Add location table, location model, location and weather controllers

---
+++ db/migrate/20200211130415_create_locations.rb
@@ -0,0 +1,15 @@
+class CreateLocations < ActiveRecord::Migration[5.2]
+  disable_ddl_transaction!
+
+  def change
+    create_table :locations do |t|
+      t.citext :title
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+
+    add_index :locations, %i[latitude longitude], algorithm: :concurrently
+  end
+end
